01d47c7d78cabc25a90870895b978a2ea7715b3e
Jens Pitkanen <jens@neon.moe>
2019-11-15 20:09:41 +0200

    Remove #[inline]s from sprite.rs
    Turns out, they were *bad* for performance. Not by much, but still.
    Weird, how compilers and cpus work.

--- src/sprite.rs
+++ src/sprite.rs
@@ -16,7 +16,6 @@ pub struct Sprite<'a, 'b> {
 }
 
 impl<'a, 'b> Sprite<'a, 'b> {
-    #[inline]
     pub(crate) fn create(
         renderer: &'a mut Renderer,
         call: &'b DrawCallHandle,
@@ -36,7 +35,6 @@ impl<'a, 'b> Sprite<'a, 'b> {
 
     /// Renders the quad specified by this struct using the given draw
     /// call and z coordinate. Smaller Z-values are rendered on top.
-    #[inline]
     pub fn finish(self) {
         if let Some(area) = self.clip_area {
             self.renderer.draw_quad_clipped(
@@ -62,7 +60,6 @@ impl<'a, 'b> Sprite<'a, 'b> {
 
     /// Specifies the screen coordinates (in logical pixels) where the
     /// quad is drawn.
-    #[inline]
     pub fn with_coordinates<R: Into<Rect>>(mut self, rect: R) -> Sprite<'a, 'b> {
         self.coords = rect.into().into_corners();
         self
@@ -70,7 +67,6 @@ impl<'a, 'b> Sprite<'a, 'b> {
 
     /// Specifies the screen coordinates (in *physical* pixels) where
     /// the quad is drawn.
-    #[inline]
     pub fn with_physical_coordinates<R: Into<Rect>>(mut self, rect: R) -> Sprite<'a, 'b> {
         let (x0, y0, x1, y1) = rect.into().into_corners();
         let df = self.renderer.dpi_factor;
@@ -80,7 +76,6 @@ impl<'a, 'b> Sprite<'a, 'b> {
 
     /// Specifies the texture coordinates (in actual pixels, in the
     /// texture's coordinate space) from where the quad is sampled.
-    #[inline]
     pub fn with_texture_coordinates<R: Into<Rect>>(mut self, rect: R) -> Sprite<'a, 'b> {
         let (tw, th) = self.renderer.get_texture_size(self.call);
         let (tw, th) = (tw as f32, th as f32);
@@ -96,7 +91,6 @@ impl<'a, 'b> Sprite<'a, 'b> {
     /// This might help you with weird visual glitches, especially if
     /// you're trying to render quads that have the same physical
     /// pixel size as the texture it's sampling.
-    #[inline]
     pub fn with_pixel_alignment(mut self) -> Sprite<'a, 'b> {
         let (x0, y0, x1, y1) = self.coords;
         let dpi_factor = self.renderer.dpi_factor;
@@ -107,7 +101,6 @@ impl<'a, 'b> Sprite<'a, 'b> {
     }
 
     /// Specifies the texture coordinates (as UVs) from where the quad is sampled.
-    #[inline]
     pub fn with_uvs<R: Into<Rect>>(mut self, rect: R) -> Sprite<'a, 'b> {
         self.texcoords = rect.into().into_corners();
         self
@@ -115,14 +108,12 @@ impl<'a, 'b> Sprite<'a, 'b> {
 
     /// Specifies the clip area. Only the parts that overlap between
     /// the clip area and this quad are rendered.
-    #[inline]
-    pub fn with_clip_area(mut self, rect: Rect) -> Sprite<'a, 'b> {
-        self.clip_area = Some(rect.into_corners());
+    pub fn with_clip_area<R: Into<Rect>>(mut self, rect: R) -> Sprite<'a, 'b> {
+        self.clip_area = Some(rect.into().into_corners());
         self
     }
 
     /// Specifies the color tint of the quad.
-    #[inline]
     pub fn with_color(mut self, red: f32, green: f32, blue: f32, alpha: f32) -> Sprite<'a, 'b> {
         self.color = (red, green, blue, alpha);
         self
@@ -130,7 +121,6 @@ impl<'a, 'b> Sprite<'a, 'b> {
 
     /// Specifies the rotation (in radians) and pivot (which is
     /// relative to the quad's `x` and `y` coordinates) of the quad.
-    #[inline]
     pub fn with_rotation(mut self, rotation: f32, pivot_x: f32, pivot_y: f32) -> Sprite<'a, 'b> {
         self.rotation = (rotation, pivot_x, pivot_y);
         self
